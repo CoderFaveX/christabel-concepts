25791174532b5ece032a202749129438b9377379
CoderFaveX <faveejiofor2009@gmail.com>
2024-11-24 01:07:36 +0100

    commit next section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,13 @@
 import "../css/Home.css";
 import bgVid from "../assets/bg.mp4";
 import { useNavigate } from "react-router-dom";
-// import manOnLaptop from "../assets/man-on-laptop.jpg";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faCheckToSlot, faPenRuler, faShip } from "@fortawesome/free-solid-svg-icons";
+import manOnLaptop from "../assets/man-on-laptop.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCheckToSlot,
+  faPenRuler,
+  faShip,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -26,34 +30,43 @@ const Home = () => {
           </button>
         </div>
       </header>
-      {/* <section className="introSlide">
+      <section className="introSlide">
         <div className="imgTact">
           <img src={manOnLaptop} />
         </div>
         <div className="description">
           <h1>Living From The Firm That Cares</h1>
-          Redefining real estate with a focus on care and commitment. Whether
-          buying, selling, or leasing, we prioritize your comfort and
-          satisfaction, delivering solutions that truly feel like home.
+          <p>
+            Redefining real estate with a focus on care and commitment. Whether
+            buying, selling, or leasing, we prioritize your comfort and
+            satisfaction, delivering solutions that truly feel like home.
+          </p>
+          <ul>
+            <li>
+              <FontAwesomeIcon icon={faShip} />
+              <span>
+                <b>Client-Centered Solutions</b>: Tailored real estate services
+                designed to meet your unique needs and preferences.
+              </span>
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faCheckToSlot} />
+              <span>
+                <b>Expertise You Can Trust</b>: Years of experience ensuring smooth
+                transactions for buying, selling, and leasing properties.
+              </span>
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faPenRuler} />
+              <span>
+                <b>Commitment to Excellence</b>: Delivering reliable, professional, and
+                results-driven services with your satisfaction as our priority.
+              </span>
+            </li>
+          </ul>
+          <button onClick={() => navigate("/about")}>Learn More</button>
         </div>
-        <ul>
-          <li>
-            <FontAwesomeIcon icon={faShip} />
-            Client-Centered Solutions: Tailored real estate services designed to
-            meet your unique needs and preferences.
-          </li>
-          <li>
-            <FontAwesomeIcon icon={faCheckToSlot} />
-            Expertise You Can Trust: Years of experience ensuring smooth
-            transactions for buying, selling, and leasing properties.
-          </li>
-          <li>
-            <FontAwesomeIcon icon={faPenRuler} />
-            Commitment to Excellence: Delivering reliable, professional, and
-            results-driven services with your satisfaction as our priority.
-          </li>
-        </ul>
-      </section> */}
+      </section>
     </>
   );
 };
